10de7bfb90e0de96870c30e9b5f06cd1c27df63b
minseongkim <yssj2049@gmail.com>
2023-06-15 02:11:58 +0900

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
 const API = {
-  LOGIN: `/login`,
-  MAJOR: `/major`,
-  FAVORITE_MAJOR: `/favoriteMajor`,
-  FAVORITE: `/favorite`,
-  BOARD: `/board`,
-  SEARCH: `/search`,
-  MORE: `/more`,
+  LOGIN: `/api/localhost/login`,
+  MAJOR: `/api/localhost/major`,
+  FAVORITE_MAJOR: `/api/localhost/favoriteMajor`,
+  FAVORITE: `/api/localhost/favorite`,
+  BOARD: `/api/localhost/board`,
+  SEARCH: `/api/localhost/search`,
+  MORE: `/api/localhost/more`,
 }
 
 export default API
